711ebb130056bfa864c61d9f2072b7811c9ba4c0
Maksim Zarutski <zarutski93maksim@gmail.com>
2025-03-05 18:22:43 +0300

    initial commit

---
+++ src/main/java/com/recap/self/rest/measurement/MeasurementApplication.java
@@ -0,0 +1,13 @@
+package com.recap.self.rest.measurement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeasurementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeasurementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recap/self/rest/measurement/MeasurementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recap.self.rest.measurement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeasurementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
